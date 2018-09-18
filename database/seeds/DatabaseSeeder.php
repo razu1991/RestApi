ebab398aa47a07185bff675431a9c96e113001ac
shahnaouzrazu21@gmail.com <shahnaouzrazu21@gmail.com>
2018-09-18 22:56:46 +0600

    factory & data seed with database table product & review

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
+
     /**
      * Seed the application's database.
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run() {
+//        $this->call(UsersTableSeeder::class);
+        factory(App\Model\Product::class, 50)->create();
+        factory(App\Model\Review::class, 300)->create();
     }
+
 }
