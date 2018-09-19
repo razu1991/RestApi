48e1b326437e88f186e1dafcd34854ddac410179
shahnaouzrazu21@gmail.com <shahnaouzrazu21@gmail.com>
2018-09-19 19:13:03 +0600

    product resource created

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductCollection extends ResourceCollection
-{
+class ProductCollection extends JsonResource {
+
     /**
      * Transform the resource collection into an array.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return array
      */
-    public function toArray($request)
-    {
-        return parent::toArray($request);
+    public function toArray($request) {
+        return [
+            'name' => $this->name,
+            'total_price' => round((1 - ($this->discount / 100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'link' => route('products.show', $this->id)
+            ]
+        ];
+        
     }
+
 }
